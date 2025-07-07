b975f1ec5a22c4cdc6d6bacedf3372bc46b4ac46
Shreyas <shreyasgolasangi303@gmail.com>
2025-07-07 16:58:33 +0530

    implemented transmission and reception api's

--- Src/stm32f407xx_spi_driver.c
+++ Src/stm32f407xx_spi_driver.c
@@ -76,29 +76,79 @@ void SPI_DeInit(SPI_RegDef_t *pSPIx)
     }
 }
 
+uint8_t SPI_GetFlagStatus(SPI_RegDef_t *pSPIx, uint8_t FlagName)
+{
+    return ((pSPIx->SR >> FlagName) & 0x1);
+}
+
 /* SPI_SendData() logic:
 
 step 1: check if len == 0
-step 2: if len != 0 then wait unitl tx buffer is empty
+step 2: while len != 0 then wait unitl tx buffer is empty
 step 3: check if dff is 8 or 16
 step 4: if tx buffer is empty then push data into the data register + increment the buffer address
 step 5: len -- or len -- -- depending of dff
 
 */
 
-/* 
+/*
 
-remember firmware cannot directly acces buffers, it has to go through data registers. the flow is 
+remember firmware cannot directly acces buffers, it has to go through data registers. the flow is
 firmware->data registers-> APB bus-> tx/rx buffer
 
 */
 
-void SPI_SendData(SPI_RegDef_t *pSPIx, uint8_t *pTxBuffer, uint32_t length){
+void SPI_SendData(SPI_RegDef_t *pSPIx, uint8_t *pTxBuffer, uint32_t length) // length of msg being sent (int bytes)
+{
+    while (length)
+    {
+        while (SPI_GetStatusFlag(pSPIx, SPI_FLAG_TXE) == 0)
+            ; // wait until the tx buffer is empty
 
-
+        if ((pSPIx->SR >> SPI_CR1_DFF) & 0x1)
+        {                                         // 16 bit format
+            pSPIx->DR = *((uint16_t *)pTxBuffer); // dereferencing a typecasted pointer
+            length--;
+            length--;
+            (uint16_t *)pTxBuffer++;
+        }
+        else
+        {
+            pSPIx->DR = *(pTxBuffer);
+            length--;
+            pTxBuffer++;
+        }
+    }
 }
 
+/* SPI_ReceiveData logic: (similar to SPI_SendData)
 
-uint8_t SPI_GetStatusFlag(SPI_RegDef_t *pSPIx, uint8_t FlagName){
-    return ((pSPIx->SR >> FlagName) & 0x1);
+step 1: while len != 0
+step 2: wait until rx buffer is not empty / until spi as received something
+step 3: check dataframe format expected 8/16
+step 4: if 16, read 2 bytes of data and increment else read 1 byte of data and increment
+step 5: len--; len-- or len--
+
+*/
+
+void SPI_ReceiveData(SPI_RegDef_t *pSPIx, uint8_t *pRXBuffer, uint32_t length)
+{
+    while (length)
+    {
+        while (SPI_GetFlagStatus(pSPIx, SPI_FLAG_RXNE) == 0)
+            ;
+        if ((pSPIx->CR1 >> SPI_CR1_DFF) & 0x1)
+        {
+            *((uint16_t *)pRXBuffer) = pSPIx->DR;
+            length--;
+            length--;
+            (uint16_t *)pRXBuffer++;
+        }
+        else
+        {
+            *(pRXBuffer) = pSPIx->DR;
+            length--;
+            pRXBuffer++;
+        }
+    }
 }
