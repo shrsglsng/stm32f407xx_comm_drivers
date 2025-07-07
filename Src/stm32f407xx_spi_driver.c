73d18f9654aaf184e1129102a2d51ad3bba19347
Shreyas <shreyasgolasangi303@gmail.com>
2025-07-07 13:17:43 +0530

    implemented spi init deinit functions

---
+++ Src/stm32f407xx_spi_driver.c
@@ -0,0 +1,104 @@
+/* currently supports 3 spi instances */
+
+#include "stm32f407xx.h"
+
+void SPI_PeriClkInit(SPI_RegDef_t *pSPIx, uint8_t EnOrDis)
+{
+    if (pSPIx == SPI1)
+    {
+        EnOrDis == ENABLE ? SPI1_CLK_EN() : SPI1_CLK_DIS();
+    }
+    else if (pSPIx == SPI2)
+    {
+        EnOrDis == ENABLE ? SPI2_CLK_EN() : SPI2_CLK_DIS();
+    }
+    else if (pSPIx == SPI3)
+    {
+        EnOrDis == ENABLE ? SPI3_CLK_EN() : SPI3_CLK_DIS();
+    }
+}
+
+void SPI_Init(SPI_Handle_t *SPI_Handle)
+{
+
+    /* configuring spi cr1 register */
+
+    SPI_Handle->pSPIx->CR1 &= ~(1 << SPI_CR1_MSTR);
+    SPI_Handle->pSPIx->CR1 |= (SPI_Handle->SPI_Config->SPI_DeviceMode << SPI_CR1_MSTR);
+
+    if (SPI_Handle->SPI_Config->SPI_BusConfig == SPI_BUS_CONFIG_FD)
+    {
+        // bidimode re\g must be cleared
+        SPI_Handle->pSPIx->CR1 &= ~(1 << SPI_CR1_BIDIMODE);
+    }
+    else if (SPI_Handle->SPI_Config->SPI_BusConfig == SPI_BUS_CONFIG_HD)
+    {
+        // bidimode must be set
+        SPI_Handle->pSPIx->CR1 |= (1 << SPI_CR1_BIDIMODE);
+    }
+    else if (SPI_Handle->SPI_Config->SPI_BusConfig == SPI_BUS_CONFIG_SIMPLEX_RXONLY)
+    {
+        // to achieve this transition from spi full duplex to rx only bt clearing bidimode + setting rxonly reg
+
+        SPI_Handle->pSPIx->CR1 &= ~(1 << SPI_CR1_BIDIMODE);
+        SPI_Handle->pSPIx->CR1 |= (1 << SPI_CR1_RXONLY);
+    }
+
+    SPI_Handle->pSPIx->CR1 &= ~(1 << SPI_CR1_BR);
+    SPI_Handle->pSPIx->CR1 |= (SPI_Handle->SPI_Config->SPI_BusConfig << SPI_CR1_BR);
+
+    SPI_Handle->pSPIx->CR1 &= ~(1 << SPI_CR1_DFF);
+    SPI_Handle->pSPIx->CR1 |= (SPI_Handle->SPI_Config->SPI_DFF << SPI_CR1_DFF);
+
+    SPI_Handle->pSPIx->CR1 &= ~(1 << SPI_CR1_CPOL);
+    SPI_Handle->pSPIx->CR1 |= (SPI_Handle->SPI_Config->SPI_CPOL << SPI_CR1_CPOL);
+
+    SPI_Handle->pSPIx->CR1 &= ~(1 << SPI_CR1_CPHA);
+    SPI_Handle->pSPIx->CR1 |= (SPI_Handle->SPI_Config->SPI_CPHA << SPI_CR1_CPHA);
+
+    SPI_Handle->pSPIx->CR1 &= ~(1 << SPI_CR1_SSM);
+    SPI_Handle->pSPIx->CR1 |= (SPI_Handle->SPI_Config->SSM << SPI_CR1_SSM);
+}
+
+void SPI_DeInit(SPI_RegDef_t *pSPIx)
+{
+    if (pSPIx == SPI1)
+    {
+        SPI1_RESET();
+    }
+    else if (pSPIx == SPI2)
+    {
+        SPI2_RESET();
+    }
+    else if (pSPIx == SPI3)
+    {
+        SPI3_RESET();
+    }
+}
+
+/* SPI_SendData() logic:
+
+step 1: check if len == 0
+step 2: if len != 0 then wait unitl tx buffer is empty
+step 3: check if dff is 8 or 16
+step 4: if tx buffer is empty then push data into the data register + increment the buffer address
+step 5: len -- or len -- -- depending of dff
+
+*/
+
+/* 
+
+remember firmware cannot directly acces buffers, it has to go through data registers. the flow is 
+firmware->data registers-> APB bus-> tx/rx buffer
+
+*/
+
+void SPI_SendData(SPI_RegDef_t *pSPIx, uint8_t *pTxBuffer, uint32_t length){
+
+
+}
+
+
+uint8_t SPI_GetStatusFlag(SPI_RegDef_t *pSPIx, uint8_t FlagName){
+    return ((pSPIx->SR >> FlagName) & 0x1);
+}
